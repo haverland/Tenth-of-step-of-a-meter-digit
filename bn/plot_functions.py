561091f4ff07dc1e2c42589c8265ff9badfeb8d2
Frank Haverland <fspapaping@googlemail.com>
2022-05-03 15:50:42 +0200

    train as regression sin cos

---
+++ bn/plot_functions.py
@@ -0,0 +1,78 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from bn.class_encoding import sin_cos_encoding_revert
+
+def plot_dataset(images, labels, columns=12, rows=5):
+
+    fig = plt.figure(figsize=(18, 10))
+    columns = 12
+    rows = 5
+
+    for i in range(1, columns*rows +1):
+        if (i>=len(labels)):
+            break
+        fig.add_subplot(rows, columns, i)
+        plt.axis("off")
+        plt.title(labels[i-1])  # set title
+        plt.imshow((images[i-1]), aspect='auto')
+        plt.tight_layout()
+    plt.show()
+
+def plot_dataset_it(data_iter, columns=12, rows=5):
+
+    fig = plt.figure(figsize=(18, 10))
+    columns = 12
+    rows = 5
+
+    for i in range(1, columns*rows +1):
+        img, label = data_iter.next()
+        fig.add_subplot(rows, columns, i)
+        plt.axis("off")
+        plt.title(round(sin_cos_encoding_revert(label[0]*10),1))  # set title
+        plt.imshow((img[0].astype(int)))
+    plt.show()
+
+def plot_acc_loss(history, modelname="modelname"):
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.suptitle(modelname)
+    fig.set_figwidth(15)
+
+    if "loss" in history.history:
+        ax1.plot(history.history['loss'])
+    if "accuracy" in history.history:
+        ax2.plot(history.history['accuracy'])
+    if "val_loss" in history.history:
+        ax1.plot(history.history['val_loss'])
+    if "val_accuracy" in history.history:
+        ax2.plot(history.history['val_accuracy'])
+    if "student_loss" in history.history:
+        ax1.plot(history.history['student_loss'])
+    if "sparse_categorical_accuracy" in history.history:
+        ax2.plot(history.history['sparse_categorical_accuracy'])
+    if "val_sparse_categorical_accuracy" in history.history:
+        ax2.plot(history.history['val_sparse_categorical_accuracy'])
+    if "student_accuracy" in history.history:
+        ax2.plot(history.history['student_accuracy'])
+    if "val_student_accuracy" in history.history:
+        ax2.plot(history.history['val_student_accuracy'])
+    if "distillation_loss" in history.history:
+        ax1.plot(history.history['distillation_loss'])
+
+    ax1.set_title('model loss')
+    ax1.set_ylabel('loss')
+    ax1.set_xlabel('epoch')
+    ax2.set_ylabel('accuracy')
+    ax2.set_xlabel('epoch')
+    ax1.legend(['train','eval'], loc='upper left')
+    axes = plt.gca()
+    axes.set_ylim([0.92,1])
+    plt.show()
+
+def plot_test_divergence(out_sin, out_cos):
+    plt.plot(out_sin)
+    plt.plot(out_cos)
+    plt.title('Result')
+    plt.ylabel('value')
+    plt.xlabel('#Picture')
+    plt.legend(['sin', 'cos'], loc='lower left')
+    plt.show()

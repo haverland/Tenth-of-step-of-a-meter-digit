1cf8af8830251e0874b2f9676c9fedfc050cea0e
Frank Haverland <fspapaping@googlemail.com>
2022-06-08 18:08:40 +0200

    new run of eff100 and new bigger cnn

---
+++ b2n/models/cnn32.py
@@ -0,0 +1,38 @@
+from tensorflow import keras
+from keras import Sequential
+from keras.layers import Dense, InputLayer, Conv2D, MaxPool2D, Flatten, Dropout, Activation
+from keras.layers import BatchNormalization, Input, LeakyReLU
+
+
+
+def VGG_like(input_shape, nb_classes):
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3,3), padding='same', input_shape=input_shape, activation='relu'))
+    model.add(Conv2D(32, (3,3), padding='same', activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(0.1))
+
+    model.add(Conv2D(64, (3,3), padding='same', activation='relu'))
+    model.add(Conv2D(64, (3,3), padding='same', activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(128, (3,3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3,3), padding='same', activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(256, (3,3), padding='same', activation='relu'))
+    model.add(Conv2D(256, (3,3), padding='same', activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(0.4))
+
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.4))
+    model.add(Dense(nb_classes, activation=None))
+    return model
